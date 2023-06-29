328c69bdaa6c9828cf706e2bda9e922e9aff37df
Kartik-Rao11 <kartik.rao29@gmail.com>
2023-06-29 16:08:09 +0530

    DashBoard Completed

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PixIcon from "@mui/icons-material/Pix";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard"); // for selecting the page on dashboard
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}> 
+      {/* LEFT SIDE */}
+      <FlexBetween gap="0.75rem">
+        <PixIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          Finance Dash Board
+        </Typography>
+      </FlexBetween>
+
+      {/* RIGHT SIDE */}
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}> {/** for choosing the dashboard */}
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}> {/** for choosing the predictions page */}
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            predictions
+          </Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
